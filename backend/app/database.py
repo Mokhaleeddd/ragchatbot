d787bdf88945f37b5d554b9a25dcfac95b777609
Mokhaleeddd <mohamed.elsersyy@outlook.com>
2025-08-10 22:01:51 +0300

    Delete backend directory

--- backend/app/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql+psycopg2://postgres:12345678@localhost:5432/chatbot_db"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
